69c2b9f0d7df00811481e94c04ddd7fd8338ea88
Jonas Karlsson <jonas.karlsson@stim.se>
2014-11-27 22:22:59 +0100

    Added primitive mock for user table

--- models/users_mock.go
+++ models/users_mock.go
@@ -3,6 +3,10 @@ package models
 type mockUserService struct {}
 
 func (mus mockUserService) Retrieve(user *User, id int) error {
+    if id != 1 {
+        return ErrNotFound
+    }
+
     user.Id = 1
     user.Deleted = false
     user.Email = "user1@example.com"
@@ -12,13 +16,39 @@ func (mus mockUserService) Retrieve(user *User, id int) error {
 }
 
 func (mus mockUserService) RetrieveSet (users *[]User) error {
+    user1 := User{
+        UserFields{
+            Id: 1,
+            Deleted: false,
+            Email: "user1@example.com",
+            Name: "User Numberone",
+        },
+    }
+
+    user2 := User{
+        UserFields{
+            Id: 2,
+            Deleted: false,
+            Email: "user2@example.com",
+            Name: "User Numbertwo",
+        },
+    }
+
+
+    *users = append(*users, user1, user2)
+
     return nil
 }
 
 func (mus mockUserService) Save (user *User) error {
+    if user.Id == 0 {
+        user.Id = 3
+    }
+
     return nil
 }
 
 func (mus mockUserService) Delete (user *User) error {
+    user.Deleted = true
     return nil
 }
