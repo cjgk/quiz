ea5569342220d2d3b70cac32ebd1a6d45c2b108d
Jonas Karlsson <jonas.karlsson@stim.se>
2014-11-27 19:59:23 +0100

    renamed main file

---
+++ quiz.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/cjgk/gorptest/controllers"
+	"github.com/cjgk/gorptest/models"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	// Set up Gorp
+	dbmap := models.InitDb()
+	defer dbmap.Db.Close()
+
+	// Set up Table services
+	tableServices := models.InitTableServices(dbmap)
+
+	// Create controllers and add Services to them
+	users := &controllers.UserController{Services: &tableServices}
+
+	// Set up router
+	router := mux.NewRouter()
+	router.StrictSlash(false)
+
+	router.HandleFunc("/users", users.Action(users.Index)).Methods("GET")
+	router.HandleFunc("/users/{key}", users.Action(users.Get)).Methods("GET")
+	router.HandleFunc("/users", users.Action(users.Post)).Methods("POST")
+	router.HandleFunc("/users/{key}", users.Action(users.Put)).Methods("PUT")
+	router.HandleFunc("/users/{key}", users.Action(users.Delete)).Methods("DELETE")
+
+	http.Handle("/", router)
+
+	http.ListenAndServe(":3001", nil)
+}
