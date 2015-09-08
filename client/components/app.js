a350eaf5e0e0539ac76a4cf8d10fdff435f4c17c
Jonas Karlsson <jonas.karlsson@stim.se>
2015-09-08 12:34:28 +0000

    rename main -> app

--- client/components/app.js
+++ client/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';  
 import {RouteHandler, Link} from 'react-router';
 
-class Main extends React.Component {  
+class App extends React.Component {  
   render() {
     return (
       <div>
@@ -12,4 +12,4 @@ class Main extends React.Component {
   }
 }
 
-export default Main;  
+export default App;
