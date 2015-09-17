254165665ebce553d14a809148d5cbc1dfdf0e9b
Jonas Karlsson <jonas.karlsson@stim.se>
2015-09-17 08:28:17 +0000

    Moved from scss to less

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js?$/, exclude: /node_modules/, loader: 'babel?cacheDirectory'},
-      {test: /\.scss$/, loader: 'style!css!sass'}
+      {test: /\.less$/, loader: 'style!css!less'}
     ]
   },
   plugins: [
